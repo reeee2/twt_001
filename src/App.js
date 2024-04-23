b0c8602dd2101978ce8bb510fbdceb36f1143853
h-kanazawa <h-kanazawa@himeginsoft.co.jp>
2024-04-23 17:31:55 +0900

    初回コミット

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import ToDoListItem from "./ToDoListItem.js"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <ToDoListItem
+            title="ホームページ作成"
+            description="サークルホームページを今週中に作成する"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
